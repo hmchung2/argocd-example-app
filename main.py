29b79c276211550b8d9d637c8bfb1da323bf35cb
정해명 <hm@jeonghaemyeong-ui-MacBookPro.local>
2025-10-08 11:02:52 +0900

    add http server

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-print('Hello ArgoCD')
+from http.server import SimpleHTTPRequestHandler, HTTPServer
+import os
+PORT = int(os.getenv("PORT", "8080"))
+class H(SimpleHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200); self.end_headers(); self.wfile.write(b"ok\n")
+HTTPServer(("", PORT), H).serve_forever()
